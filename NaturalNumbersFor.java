9e16f7488801c52c58eea16f58e1e935bba360a2
ram-221 <rameshmalli857.com>
2022-08-28 22:51:06 +0530

    Sum Of n Natural Numbers using For Loop

---
+++ NaturalNumbersFor.java
@@ -0,0 +1,17 @@
+package com.bridgelabday2;
+import java.util.Scanner;
+
+public class NaturalNumbersFor {
+
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter the number: ");
+	        int a = sc.nextInt();
+	        int sum =0;
+	        for (int i = 1; i <=a ; i++) {
+	            sum +=i;
+	        }
+	        System.out.println("The result is: "+sum);
+	    }
+
+}
